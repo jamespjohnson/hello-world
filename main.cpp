b9e93dd3f3f653fc547c67c156125ab442c53c7d
jamespjohnson <45546400+jamespjohnson@users.noreply.github.com>
2021-01-27 18:46:06 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,7 +4,12 @@
 ================
 #include <iostream>
 using namespace std;
+int hello2Function(string name2);
 int main()
 {
-
+  string name1="your name1";
+  hello1Function(name1);
+  string name2="your name2";
+  hello2Function(name2);
+  return 0;
 }
